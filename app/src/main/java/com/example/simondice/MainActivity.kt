ea823345869cef813c3827b1eea48398d9b8e92a
alex0 <aquintafarina@danielcastelao.org>
2022-11-11 16:26:20 +0100

    Creacion del metodo onCreate y onClick

--- app/src/main/java/com/example/simondice/MainActivity.kt
+++ app/src/main/java/com/example/simondice/MainActivity.kt
@@ -9,6 +9,7 @@ import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.alejandro.simon_dice_alejandro.R
 import kotlinx.coroutines.*
 import java.util.*
 
@@ -20,7 +21,7 @@ class MainActivity : AppCompatActivity() {
     private var contador=0
     private var puntuacion=0
     private var booleanBotones: Boolean = true
-    var numerointroducido:Int = 0
+    var numeroIntroducido:Int = 0
 
     lateinit var btnRojo:Button
     lateinit var btnAzul:Button
@@ -30,4 +31,35 @@ class MainActivity : AppCompatActivity() {
     lateinit var score:TextView
     lateinit var record: TextView
     var puntuacion2: Int=0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        btnIniciar= findViewById(R.id.btn_Inicio)
+        btnRojo=findViewById(R.id.btn_rojo)
+        btnAzul=findViewById(R.id.btn_azul)
+        btnVerde=findViewById(R.id.btn_verde)
+        btnAmarillo=findViewById(R.id.btn_amarillo)
+        score = findViewById(R.id.score)
+        record = findViewById(R.id.record)
+        loadRecord()
+        btnIniciar.setOnClickListener(object : View.OnClickListener{
+
+
+            override fun onClick(p0: View?) {
+                inicio()
+                btnIniciar.setVisibility(View.GONE)
+                Log.d("Lehasdadoclick","adsf")
+            }
+        })
+
     }
+
+    private fun inicio(){
+        colorRandom()
+    }
+
+
+}
+
